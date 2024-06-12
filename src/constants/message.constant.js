54f0adb4a7dc8ba257eddc53dfe0578b44125b4e
Changjun Yeo <lunaradio24@gmail.com>
2024-06-13 04:33:35 +0900

    Refactor: 역할 인증 미들웨어 #8

--- src/constants/message.constant.js
+++ src/constants/message.constant.js
@@ -29,7 +29,7 @@ export const MESSAGES = {
         INVALID: '인증 정보가 유효하지 않습니다.',
       },
       ROLE: {
-        BLOCKED_USER: '접근 권한이 없습니다.',
+        NO_ACCESS_RIGHT: '접근 권한이 없습니다.',
       },
     },
     SIGN_UP: {
